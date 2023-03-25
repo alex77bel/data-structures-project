cb358cf88c65fcdc41f2aceb6431e35b4e35b89d
aleksandr churilin <alex77bel@yandex.ru>
2023-03-26 00:34:09 +0300

    - реализован класс Node и LinkedList в src/linked_list.py - добавлены тесты для классов Node и LinkedList в tests/test_linked_list.py`

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,18 +1,39 @@
 class Node:
     """Класс для узла односвязного списка"""
-    pass
+
+    def __init__(self, data, next_node):
+        """
+        Конструктор класса Node
+        :param data: данные, которые будут храниться в узле
+        """
+        self.data = data
+        self.next_node = next_node
 
 
 class LinkedList:
     """Класс для односвязного списка"""
 
+    def __init__(self):
+        """Конструктор класса LinkedList"""
+        self.head = self.tail = None
+
     def insert_beginning(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        pass
+        node = Node(data, None)
+        if self.head is None:
+            self.head = self.tail = node
+        else:
+            node.next_node = self.head
+            self.head = node
 
     def insert_at_end(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        pass
+        node = Node(data, None)
+        if self.head is None:
+            self.head = self.tail = node
+        else:
+            self.tail.next_node = node
+            self.tail = node
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
@@ -22,8 +43,8 @@ class LinkedList:
 
         ll_string = ''
         while node:
-            ll_string += f' {str(node.data)} ->'
+            ll_string += f'{str(node.data)} -> '
             node = node.next_node
 
-        ll_string += ' None'
+        ll_string += 'None'
         return ll_string
--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,1 +1,33 @@
-"""Здесь надо написать тесты с использованием unittest для модуля linked_list."""
+import unittest
+from src.linked_list import Node, LinkedList
+
+
+class TestNode(unittest.TestCase):
+
+    def setUp(self):
+        self.node = Node({'id': 1}, None)
+
+    def test_node_init(self):
+        self.assertEqual(self.node.data, {'id': 1})
+        self.assertEqual(self.node.next_node, None)
+
+
+class TestLinkedList(unittest.TestCase):
+
+    def setUp(self):
+        self.LinkedList = LinkedList()
+
+    def test_LinkedList_init(self):
+        self.assertEqual(self.LinkedList.tail, None)
+        self.assertEqual(self.LinkedList.head, None)
+
+    def test_enLinkedList_str_insert_beginning_insert_at_end(self):
+        self.LinkedList.insert_beginning({'id': 1})
+        self.assertEqual(self.LinkedList.head.data, {'id': 1})
+        self.LinkedList.insert_at_end({'id': 2})
+        self.assertEqual(self.LinkedList.tail.data, {'id': 2})
+        self.LinkedList.insert_at_end({'id': 3})
+        self.assertEqual(self.LinkedList.tail.data, {'id': 3})
+        self.LinkedList.insert_beginning({'id': 0})
+        self.assertEqual(self.LinkedList.head.data, {'id': 0})
+        assert str(self.LinkedList) == "{'id': 0} -> {'id': 1} -> {'id': 2} -> {'id': 3} -> None"
